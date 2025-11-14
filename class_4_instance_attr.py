0f183d8a1bf1a088048c973461b927378563dd5c
RebecaR <rramirez2@broncos.uncfsu.edu>
2025-11-14 10:24:27 -0500

    class_3_initialization.py

--- class_4_instance_attr.py
+++ class_4_instance_attr.py
@@ -4,6 +4,10 @@ class House:
     high_school = "FV High"
 
     # TODO: create an initialization method, and three instance attributes:
-    # attribute name "size" with int value 2000
+    # attribute name "size" with int value 2500
     # attribute name "owner" from argument during class calling
     # attribute name "address" from argument during class calling
+    def __init__(self, owner, address):
+        self.size = 2500
+        self.owner = owner
+        self.address = address
