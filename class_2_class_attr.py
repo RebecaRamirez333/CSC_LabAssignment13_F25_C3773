5e22d666c919746013f2f60bace8f4a6435750cd
RebecaR <rramirez2@broncos.uncfsu.edu>
2025-11-14 10:17:52 -0500

    class_2_class_attr.py

--- class_2_class_attr.py
+++ class_2_class_attr.py
@@ -1,5 +1,9 @@
-class House:
+
     # TODO: define three class attributes:
     # attribute name "city" with string value "Cary"
     # attribute name "community" with string value "Palm River"
     # attribute name "builder" with string value "Python"
+class House:
+    city = "Cary"
+    community = "Palm River"
+    builder = "Python"
